388cd44cf9220a673e2eb6c9b06d8e6137846f1c
chenpeng <smwyzi@qq.com>
2022-04-20 22:27:03 +0800

    fix get_pack_flow function

--- src/application_protocols/awesomerpc/protocol.h
+++ src/application_protocols/awesomerpc/protocol.h
@@ -25,7 +25,7 @@ struct AwesomerpcHeader : public Logger::Loggable<Logger::Id::filter> {
   bool decode(Buffer::Instance& buffer);
   bool encode(Buffer::Instance& buffer);
 
-  uint32_t get_pack_flow() const {return _pack_len;};
+  uint32_t get_pack_flow() const {return _pack_flow;};
   uint32_t get_pack_len() const {return _pack_len;};
   int16_t get_req_cmd() const {return _req_cmd;};
   void set_pack_flow(uint32_t pack_flow) {_pack_flow = pack_flow;};
